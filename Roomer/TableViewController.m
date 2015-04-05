e76c8df25e6a78349719a958dcd7f8ad27b70816
kellybmcgee <kelly@fanpics.com>
2015-04-04 23:11:03 -0400

    Finalized JSON Parsing and Population

--- Roomer/TableViewController.m
+++ Roomer/TableViewController.m
@@ -17,6 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    // Create Authorization Token!
     
     //Create Session Configuration
     NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
@@ -24,24 +25,88 @@
     //Configure Session Authentication [HEADERS]
     [sessionConfiguration setHTTPAdditionalHeaders:@{ @"Authorization" : @"Bearer eyJhbGciOiJSUzI1NiJ9.eyJhdWQiOlsiMjY2ZWJjYzgtYjBhMS00ZjgwLWFiMzktODUwNjE5OTBmZjk0Il0sImlzcyI6Imh0dHBzOlwvXC9vaWRjLm1pdC5lZHVcLyIsImp0aSI6ImVkNTUwYjg1LTViNzYtNDExMy1hYzIxLTVhZjAxODBlNzhjYyIsImlhdCI6MTQyODEyMDI0NH0.YQFjJI0L_hkDAyOAfYME-fbCypHmSaKLAq6B0AL0GT5C9uCUZMAAHbps7gn3H12pEE0-0Me57Abl2xIYJOKnnD26SeA1hOKx2VXeoAb6UcrrFlB8akacFOOiQ38m-7aZ6TGyy5gRMe8cyqE82DNk7_c4WiItpq26xI8MchSgkY6e1cCUSpCqFYMn4BTVqbX8vsKWzt9COK6kBGrUQt-hF9CpLoS6HL3v3c6PFNKVYltT67ylN7SRIloFiv5KBdDgnHKUehWkgHCXVUcVO3TaBsnIxPiFBXcrzb4cQkxCt8HcXHwgv_025fzaB7Nrf_70FwY8PVDBn21ioPw4p5bFjw" }];
     NSURLSession *session = [NSURLSession sharedSession];
-    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:[NSURL URLWithString:@"http://mocksvc.mulesoft.com/mocks/d421fa0f-073f-4c2e-b3bf-b15e4fd62ec3/classrooms/3-333"] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-        NSLog(@"%@", json);
-    }];
-    
-    [dataTask resume];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:[NSURL URLWithString:@"http://mocksvc.mulesoft.com/mocks/d421fa0f-073f-4c2e-b3bf-b15e4fd62ec3/classrooms/"] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        //Start JSON Parsing
+        NSMutableArray *roomNumber = [[NSMutableArray alloc] init];
+        NSMutableArray *roomDescription = [[NSMutableArray alloc] init];
+        NSMutableArray *roomStatus = [[NSMutableArray alloc] init];
+        NSMutableArray *availabilityDuration = [[NSMutableArray alloc] init];
 
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+        NSMutableDictionary *roomDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&error];
+        NSLog(@"%@", roomDictionary);
+        NSArray *apiroomdata = roomDictionary[@"data"];
+        for ( NSDictionary *attributes in apiroomdata )
+        {
+            [roomNumber addObject:attributes[@"room"]];
+            NSArray *features = attributes[@"features"];
+            for ( NSDictionary *featureName in features)
+            {
+                [roomDescription addObject:featureName[@"name"]];
+            }
+            NSArray *availabilities = attributes[@"availabilities"];
+            for ( NSDictionary *availabilityStatus in availabilities)
+            {
+                if(availabilityStatus[@"available"]) {
+                    [roomStatus addObject: @"Open"];
+                    NSString *endDateAndTime = availabilityStatus[@"end"];
+                    NSCharacterSet *doNotWantT = [NSCharacterSet characterSetWithCharactersInString:@"T"];
+                    NSString *noT = [[endDateAndTime componentsSeparatedByCharactersInSet: doNotWantT] componentsJoinedByString: @" "];
+                    NSCharacterSet *doNotWantZ = [NSCharacterSet characterSetWithCharactersInString:@"Z"];
+                    NSString *noTorZ = [[noT componentsSeparatedByCharactersInSet: doNotWantZ] componentsJoinedByString: @""];
+                    noTorZ = [noTorZ stringByAppendingString:@".000"];
+                    
+                    NSString *endTime = [endDateAndTime componentsSeparatedByString:@"T"][1];
+                    NSString *hours =[endTime componentsSeparatedByString:@":"][0];
+                    NSString *minutes =[endTime componentsSeparatedByString:@":"][1];
+                    int number = [hours intValue];
+                    if (number >=4){
+                        number = number - 4;
+                    }
+                    else{
+                        number = number + 21;
+                    }
+                    NSString *amPm;
+                    if(number <12){
+                        amPm = @" am";
+                    }
+                    else {
+                        number = number - 12;
+                        amPm = @" pm";
+                    }
+                    hours = [NSString stringWithFormat:@"%d",number];
+                    NSString *finalTime = [[[[@"Until " stringByAppendingString:hours] stringByAppendingString:@":"] stringByAppendingString:minutes] stringByAppendingString:amPm];
+                    [availabilityDuration addObject: finalTime];
+                }
+                else {
+                    [roomStatus addObject: @"Closed"];
+                }
+            }
+            
+        }
+        self.RoomNumber = roomNumber;
+        self.Description = roomDescription;
+        self.Availability = roomStatus;
+        self.LengthOfAvailable = availabilityDuration;
+        
+        // 6
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+            [self.tableView reloadData];
+        });
+    }];
+    [dataTask resume];
     
+  /*
+    for ( NSDictionary *attributes in roominfo )
+    {
+        NSLog(@"Available?: %@", attributes[@"room"]);
+    }
+   */
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
-    _RoomNumber = @[@"1-123",@"1-124", @"3-345", @"2-323", @"20-234", @"30-123"];
-    _Description = @[@"20 seats, Blackboard", @"50 seats, Projector", @"20 seats, Blackboard, Projector", @"20 seats, Blackboard", @"50 seats, Projector", @"20 seats, Blackboard, Projector"];
-    _Availability = @[@"Open", @"Closed", @"Open", @"Open", @"Closed", @"Open"];
-    _LengthOfAvailable = @[@"until 7pm", @"until 5pm", @"until 6pm", @"until 7pm", @"until 5pm", @"until 6pm"];
     
+    _LengthOfAvailable = @[@"until 7pm", @"until 5pm", @"until 6pm", @"until 7pm", @"until 5pm", @"until 6pm"];
 }
 
 - (void)didReceiveMemoryWarning {
